a4398f368040b0e7f65f095d317ff0a0e7045294
DeltaOrion <specdartpokeman@gmail.com>
2022-04-18 23:56:30 +1000

    Added material completers

--- bukkit/bukkit-api/src/main/java/me/deltaorion/bukkit/command/tabcompletion/CompletersBukkit.java
+++ bukkit/bukkit-api/src/main/java/me/deltaorion/bukkit/command/tabcompletion/CompletersBukkit.java
@@ -1,13 +1,17 @@
 package me.deltaorion.bukkit.command.tabcompletion;
 
+import me.deltaorion.bukkit.item.EMaterial;
 import me.deltaorion.common.command.CommandException;
 import me.deltaorion.common.command.sent.SentCommand;
 import me.deltaorion.common.command.tabcompletion.CompletionSupplier;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class CompletersBukkit {
 
@@ -15,16 +19,57 @@ public class CompletersBukkit {
         throw new UnsupportedOperationException();
     }
 
-    public static CompletionSupplier BUKKIT_PLAYERS(Server server) {
+    public static CompletionSupplier BUKKIT_PLAYERS() {
         return new CompletionSupplier() {
             @Override
             public List<String> getCompletions(SentCommand command) throws CommandException {
                 List<String> completion = new ArrayList<>();
-                for(Player player : server.getOnlinePlayers()) {
+                for(Player player : Bukkit.getOnlinePlayers()) {
                     completion.add(player.getName());
                 }
                 return completion;
             }
         };
     }
+
+    public static CompletionSupplier EMATERIAL() {
+        return new CompletionSupplier() {
+            @Override
+            public List<String> getCompletions(SentCommand command) throws CommandException {
+                List<String> completions = new ArrayList<>();
+                for(EMaterial material : EMaterial.valuesThisVersion()) {
+                    completions.add(material.name().toLowerCase(Locale.ROOT));
+                }
+                return completions;
+            }
+        };
+    }
+
+    public static CompletionSupplier EMATERIAL_ALL_VERSION() {
+        return new CompletionSupplier() {
+            @Override
+            public List<String> getCompletions(SentCommand command) throws CommandException {
+                List<String> completions = new ArrayList<>();
+                for(EMaterial material : EMaterial.values()) {
+                    completions.add(material.name().toLowerCase(Locale.ROOT));
+                }
+                return completions;
+            }
+        };
+    }
+
+    public static CompletionSupplier MATERIAL() {
+        return new CompletionSupplier() {
+            @Override
+            public List<String> getCompletions(SentCommand command) throws CommandException {
+                List<String> completions = new ArrayList<>();
+                for(Material material : Material.values()) {
+                    completions.add(material.name().toLowerCase(Locale.ROOT));
+                }
+                return completions;
+            }
+        };
+    }
+
+
 }
